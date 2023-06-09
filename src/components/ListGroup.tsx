7c22805a3a0980388a21a783b3c17c7539f4af90
Mauricio Erazo <antonierazo13@hotmail.com>
2023-06-09 15:05:54 +0200

    item length === 0 &&

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,17 @@
 function ListGroup() {
-  const items = [
+  let items = [
     'New York',
     'San Francisco',
     'Apopa',
     'Tokio',
     'Munich'
   ]
-
+  items = []
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
